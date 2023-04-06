b7ffb4264272e7ae9b5a60691547ffb86d1f663b
dean <dean@yanadoocorp.com>
2023-04-06 14:49:31 +0900

    streaming grpc 샘플코드

--- server.go
+++ server.go
@@ -2,32 +2,199 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"github.com/JooHyeongLee/clean-gin/bootstrap"
-	pb "github.com/JooHyeongLee/clean-gin/proto/user"
+	samplePb "github.com/JooHyeongLee/clean-gin/proto/sample"
+	userPb "github.com/JooHyeongLee/clean-gin/proto/user"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/joho/godotenv"
 	"google.golang.org/grpc"
+	"google.golang.org/protobuf/proto"
+	"io"
 	"log"
+	"math"
 	"net"
+	"os"
+	"sync"
+	"time"
 )
 
 var (
-	port = flag.Int("port", 50051, "The server port")
+	port       = flag.Int("port", 50051, "The server port")
+	jsonDBFile = flag.String("json_db_file", "route_guide_db.json", "A json file containing a list of features")
 )
 
 type server struct {
-	pb.UnimplementedUserServer
+	userPb.UnimplementedUserServer
+	samplePb.UnimplementedRouteGuideServer
+	savedFeatures []*samplePb.Feature
+	mu            sync.Mutex
+	routeNotes    map[string][]*samplePb.RouteNote
 }
 
 // GetUser returns user message by user_id
-func (s *server) GetUser(ctx context.Context, req *pb.GetUserRequest) (*pb.GetUserResponse, error) {
-	var userMessage *pb.UserMessage
+func (s *server) GetUser(ctx context.Context, req *userPb.GetUserRequest) (*userPb.GetUserResponse, error) {
+	var userMessage *userPb.UserMessage
 	fmt.Println("hello Get User")
-	return &pb.GetUserResponse{
+	return &userPb.GetUserResponse{
 		UserMessage: userMessage,
 	}, nil
+}
+
+// GetFeature returns the feature at the given point.
+func (s *server) GetFeature(ctx context.Context, point *samplePb.Point) (*samplePb.Feature, error) {
+	for _, feature := range s.savedFeatures {
+		if proto.Equal(feature.Location, point) {
+			return feature, nil
+		}
+	}
+	// No feature was found, return an unnamed feature
+	return &samplePb.Feature{Location: point}, nil
+}
+
+// ListFeatures lists all features contained within the given bounding Rectangle.
+func (s *server) ListFeatures(rect *samplePb.Rectangle, stream samplePb.RouteGuide_ListFeaturesServer) error {
+	for _, feature := range s.savedFeatures {
+		if inRange(feature.Location, rect) {
+			if err := stream.Send(feature); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+// RecordRoute records a route composited of a sequence of points.
+//
+// It gets a stream of points, and responds with statistics about the "trip":
+// number of points,  number of known features visited, total distance traveled, and
+// total time spent.
+func (s *server) RecordRoute(stream samplePb.RouteGuide_RecordRouteServer) error {
+	var pointCount, featureCount, distance int32
+	var lastPoint *samplePb.Point
+	startTime := time.Now()
+	for {
+		point, err := stream.Recv()
+		if err == io.EOF {
+			endTime := time.Now()
+			return stream.SendAndClose(&samplePb.RouteSummary{
+				PointCount:   pointCount,
+				FeatureCount: featureCount,
+				Distance:     distance,
+				ElapsedTime:  int32(endTime.Sub(startTime).Seconds()),
+			})
+		}
+		if err != nil {
+			return err
+		}
+		pointCount++
+		for _, feature := range s.savedFeatures {
+			if proto.Equal(feature.Location, point) {
+				featureCount++
+			}
+		}
+		if lastPoint != nil {
+			distance += calcDistance(lastPoint, point)
+		}
+		lastPoint = point
+	}
+}
+
+// RouteChat receives a stream of message/location pairs, and responds with a stream of all
+// previous messages at each of those locations.
+func (s *server) RouteChat(stream samplePb.RouteGuide_RouteChatServer) error {
+	for {
+		in, err := stream.Recv()
+		if err == io.EOF {
+			return nil
+		}
+		if err != nil {
+			return err
+		}
+		key := serialize(in.Location)
+
+		s.mu.Lock()
+		s.routeNotes[key] = append(s.routeNotes[key], in)
+		// Note: this copy prevents blocking other clients while serving this one.
+		// We don't need to do a deep copy, because elements in the slice are
+		// insert-only and never modified.
+		rn := make([]*samplePb.RouteNote, len(s.routeNotes[key]))
+		copy(rn, s.routeNotes[key])
+		s.mu.Unlock()
+
+		for _, note := range rn {
+			if err := stream.Send(note); err != nil {
+				return err
+			}
+		}
+	}
+}
+
+// loadFeatures loads features from a JSON file.
+func (s *server) loadFeatures(filePath string) {
+	var data []byte
+	if filePath != "" {
+		var err error
+		data, err = os.ReadFile(filePath)
+		if err != nil {
+			log.Fatalf("Failed to load default features: %v", err)
+		}
+	}
+	if err := json.Unmarshal(data, &s.savedFeatures); err != nil {
+		log.Fatalf("Failed to load default features: %v", err)
+	}
+}
+
+func toRadians(num float64) float64 {
+	return num * math.Pi / float64(180)
+}
+
+// calcDistance calculates the distance between two points using the "haversine" formula.
+// The formula is based on http://mathforum.org/library/drmath/view/51879.html.
+func calcDistance(p1 *samplePb.Point, p2 *samplePb.Point) int32 {
+	const CordFactor float64 = 1e7
+	const R = float64(6371000) // earth radius in metres
+	lat1 := toRadians(float64(p1.Latitude) / CordFactor)
+	lat2 := toRadians(float64(p2.Latitude) / CordFactor)
+	lng1 := toRadians(float64(p1.Longitude) / CordFactor)
+	lng2 := toRadians(float64(p2.Longitude) / CordFactor)
+	dlat := lat2 - lat1
+	dlng := lng2 - lng1
+
+	a := math.Sin(dlat/2)*math.Sin(dlat/2) +
+		math.Cos(lat1)*math.Cos(lat2)*
+			math.Sin(dlng/2)*math.Sin(dlng/2)
+	c := 2 * math.Atan2(math.Sqrt(a), math.Sqrt(1-a))
+
+	distance := R * c
+	return int32(distance)
+}
+
+func inRange(point *samplePb.Point, rect *samplePb.Rectangle) bool {
+	left := math.Min(float64(rect.Lo.Longitude), float64(rect.Hi.Longitude))
+	right := math.Max(float64(rect.Lo.Longitude), float64(rect.Hi.Longitude))
+	top := math.Max(float64(rect.Lo.Latitude), float64(rect.Hi.Latitude))
+	bottom := math.Min(float64(rect.Lo.Latitude), float64(rect.Hi.Latitude))
+
+	if float64(point.Longitude) >= left &&
+		float64(point.Longitude) <= right &&
+		float64(point.Latitude) >= bottom &&
+		float64(point.Latitude) <= top {
+		return true
+	}
+	return false
+}
+
+func serialize(point *samplePb.Point) string {
+	return fmt.Sprintf("%d %d", point.Latitude, point.Longitude)
+}
+
+func newServer() *server {
+	s := &server{routeNotes: make(map[string][]*samplePb.RouteNote)}
+	s.loadFeatures(*jsonDBFile)
+	return s
 }
 
 func main() {
@@ -40,7 +207,8 @@ func main() {
 			log.Fatalf("failed to listen: %v", err)
 		}
 		s := grpc.NewServer()
-		pb.RegisterUserServer(s, &server{})
+		userPb.RegisterUserServer(s, &server{})
+		samplePb.RegisterRouteGuideServer(s, newServer())
 		log.Printf("server listening at %v", lis.Addr())
 		if err := s.Serve(lis); err != nil {
 			log.Fatalf("failed to serve: %v", err)
